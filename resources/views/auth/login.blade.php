ad7fc682bd92914976aea968c2f2437c2303aa67
Cristine Haber <haber650@gmail.com>
2023-09-06 19:50:43 +0800

    feat: update the logo icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MetaSys</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('/images/logos/favicon.png') }}" />
+    <link rel="shortcut icon" type="image/png" href="{{ asset('/images/logos/logo.png') }}" />
     <link rel="stylesheet" href="{{ asset('/css/styles.min.css') }}" />
 </head>
 
