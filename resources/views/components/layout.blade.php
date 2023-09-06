2671ba9b3f6777b7ca3c557bfebb073bd2b43b35
Cristine Haber <haber650@gmail.com>
2023-09-06 19:52:55 +0800

    feat: make some changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,7 +23,7 @@
             <!-- Sidebar scroll-->
             <div>
                 <div class="brand-logo d-flex align-items-center justify-content-between">
-                    <a href="./index.html" class="text-nowrap logo-img">
+                    <a href="/admin" class="text-nowrap logo-img">
                         <img src="{{ asset('/images/logos/logo.png') }}" width="180" alt="" />
                     </a>
                     <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
