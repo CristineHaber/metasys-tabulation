21a951631da4a6991fc2335922cb4d56b21a49ed
Cristine Haber <haber650@gmail.com>
2023-09-07 11:15:12 +0800

    feat: change the text to logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,8 @@
                         <div class="card mb-0">
                             <div class="card-body">
                                 <span class="text-nowrap logo-img text-center d-block py-3 w-100">
-                                    {{-- <img src="/images/logos/dark-logo.svg" width="180" alt=""> --}}
-                                    <h1>Meta<span class="text-primary">Sys</span></h1>
+                                    <img src="{{asset('images/logos/logo.png')}}" width="300" alt="">
                                 </span>
-                               <h1>Hello World</h1>
                             </div>
                         </div>
                     </div>
